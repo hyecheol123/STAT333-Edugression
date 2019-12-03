db518e3b5dc5ce0efdc457eb4189d0cb0b5bd5ca
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2019-12-02 18:32:39 -0600

    Export fipscode csv

--- Manipulated Data/fipscodes.R
+++ Manipulated Data/fipscodes.R
@@ -1,7 +1,8 @@
 
-all_fipscodes <- read.csv("ZIP-COUNTY-FIPS_2017-06.csv")
- # View(all_fipscodes)
+all_fipscodes <- read.csv("../Original Data/ZIP-COUNTY-FIPS_2017-06.csv")
+# View(all_fipscodes)
 wi_fipscodes <- all_fipscodes[which(all_fipscodes$STATE == "WI"),]
 #remove unnecessary columns
 wi_fipscodes <- subset(wi_fipscodes, select= -c(COUNTYNAME, CLASSFP, STATE)) 
 
+write.csv(wi_fipscodes,"fipscodes.csv", row.names = FALSE)
